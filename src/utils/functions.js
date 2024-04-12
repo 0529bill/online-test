231d72f609778fe885fd7c99f8bab532713d6bb7
bill <0529bilL@gmail.com>
2024-04-12 08:49:11 +0800

    fix: remove var

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,5 @@
 function addComma(num) {
-  var parts = num.toString().split(".");
+  let parts = num.toString().split(".");
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   return parts.join(".");
 }
